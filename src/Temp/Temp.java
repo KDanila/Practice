4117b51628baa56fd0b94284ae314b28856b97bd
Kuzmin Danila <bus1d0@mail.ru>
2018-03-01 21:27:59 +0300

    14

--- src/Temp/Temp.java
+++ src/Temp/Temp.java
@@ -1,21 +1,69 @@
 package Temp;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Temp {
     public static void main(String[] args) {
-        ArrayList<String> temp = new ArrayList<String>();
-        temp.add("0");
-        temp.add("1");
-        temp.add("2");
-        temp.add("3");
-        temp.add("4");
-        temp.add("5");
-        temp.add("6");
-        ListIterator<String> i = temp.listIterator();
-        while (i.hasPrevious()){
-            String t = i.previous();
-            System.out.println(t);
-        }
+        PriorityQueue pq = new PriorityQueue();
+        Task t1 = new Task("1", 9);
+        Task t2 = new Task("2", 3);
+        Task t3 = new Task("3", 2);
+        Task t4 = new Task("4", 1);
+        Task t5 = new Task("5", 5);
     }
 }
+
+class Task {
+    private String desc;
+    private int priority;
+
+    public Task(String desc, int priority) {
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
+
+
+class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<Task>();
+
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     *
+     * @param task задача
+     */
+    public void put(Task task) {
+        int numOfInput;
+        boolean flag = false;
+        int priority = task.getPriority();
+        ListIterator<Task> lit = tasks.listIterator();
+        if (tasks.size() == 0) {
+            tasks.add(task);
+        } else {
+            while (lit.hasNext()) {
+                Task t = lit.next();
+                if (t.getPriority() <= priority) {
+                    numOfInput = lit.previousIndex();
+                    flag = false;
+                }
+            }
+        }
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
+
+
