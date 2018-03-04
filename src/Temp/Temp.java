d398e3743de29a0c6ce9154cd88c6c4003f354f2
Kuzmin Danila <bus1d0@mail.ru>
2018-03-04 16:04:21 +0300

    temporary changes

--- src/Temp/Temp.java
+++ src/Temp/Temp.java
@@ -1,69 +1,83 @@
 package Temp;
 
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Temp {
     public static void main(String[] args) {
-        PriorityQueue pq = new PriorityQueue();
-        Task t1 = new Task("1", 9);
-        Task t2 = new Task("2", 3);
-        Task t3 = new Task("3", 2);
-        Task t4 = new Task("4", 1);
-        Task t5 = new Task("5", 5);
+
+        ConvertList cv = new ConvertList();
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        temp.add(1);
+        temp.add(4);
+        temp.add(3);
+        temp.add(2);
+        temp.add(1);
+        temp.add(5);
+        temp.add(1);
+        int[][] m = cv.toArray(temp, 1);
+        System.out.println(Arrays.deepToString(m));
     }
 }
 
-class Task {
-    private String desc;
-    private int priority;
-
-    public Task(String desc, int priority) {
-        this.desc = desc;
-        this.priority = priority;
-    }
-
-    public String getDesc() {
-        return desc;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-}
-
-
-class PriorityQueue {
-    private LinkedList<Task> tasks = new LinkedList<Task>();
-
+class ConvertList {
     /**
-     * Метод должен вставлять в нужную позицию элемент.
-     * Позиция определять по полю приоритет.
-     * Для вставик использовать add(int index, E value)
+     * В метод приходит двумерный массив -----> добавляем в list.
      *
-     * @param task задача
+     * @param array - int [][] - is needed to add in list.
+     * @return - list array.
      */
-    public void put(Task task) {
-        int numOfInput;
-        boolean flag = false;
-        int priority = task.getPriority();
-        ListIterator<Task> lit = tasks.listIterator();
-        if (tasks.size() == 0) {
-            tasks.add(task);
-        } else {
-            while (lit.hasNext()) {
-                Task t = lit.next();
-                if (t.getPriority() <= priority) {
-                    numOfInput = lit.previousIndex();
-                    flag = false;
-                }
+    public List<Integer> toList(int[][] array) {
+        List<Integer> toReturn = new ArrayList<Integer>();
+        for (int[] i : array) {
+            for (int j : i) {
+                toReturn.add(j);
             }
         }
+        return toReturn;
     }
 
-    public Task take() {
-        return this.tasks.poll();
+    /**
+     * метод разбивает лист на количество строк двумерного массива.
+     * В методе toArray должна есть проверка - если количество элементов не кратно количеству строк
+     * - оставшиеся значения в массиве заполнять нулями.
+     *
+     * @param list - list.
+     * @param rows - number of rows.
+     * @return - int [][] array.
+     */
+    public int[][] toArray(List<Integer> list, int rows) {
+
+        int[][] toReturn = new int[rows][rows];
+//        Arrays.fill(toReturn, 0);
+        int i = 0;
+        for (Integer li : list) {
+            if (i < rows * rows) {
+                toReturn[i / rows][i % rows] = li;
+                i++;
+            }
+        }
+
+        return toReturn;
+
+
     }
 }
 
-
+/**
+ * Вам необходимо создать класс ConvertList. Внутри методов использовать foreach.
+ * <p>
+ * В нём написать 2 метода:
+ * <p>
+ * public List<Integer> toList (int[][] array) {} - в метод приходит двумерный массив целых чисел, необходимо пройтись
+ * по всем элементам массива и добавить их в List<Integer>.
+ * <p>
+ * public int[][] toArray (List<Integer> list, int rows) {} - метод toArray должен равномерно разбить лист на количество
+ * строк двумерного массива. В методе toArray должна быть проверка - если количество элементов не кратно количеству строк
+ * - оставшиеся значения в массиве заполнять нулями.
+ * <p>
+ * Например в результате конвертации листа со значениями (1,2,3,4,5,6,7) с разбиением на 3 строки должен получиться
+ * двумерный массив {{1, 2, 3} {4, 5, 6} {7, 0 ,0}}
+ */
